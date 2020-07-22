7ac0d5ec22a470e08d25b3b777ad8e16c66d3316
Julius Plehn <juplehn@me.com>
2020-07-23 01:59:16 +0200

    cleanup

--- lib/core/hashing/jhash_sha256.c
+++ lib/core/hashing/jhash_sha256.c
@@ -26,44 +26,52 @@
 
 #include <hashing/jhash_sha256.h>
 
-
-void* sha256_context(void){
-	GChecksum *mdctx;
+void*
+sha256_context(void)
+{
+	GChecksum* mdctx;
 	mdctx = g_checksum_new(G_CHECKSUM_SHA256);
 	return mdctx;
 }
 
-int sha256_init(void *ctx){
+int
+sha256_init(void* ctx)
+{
 	g_checksum_reset(ctx);
 	return 0;
 }
 
-int sha256_update(void *ctx, const void *data, size_t length){
+int
+sha256_update(void* ctx, const void* data, size_t length)
+{
 	g_checksum_update(ctx, data, length);
 	return 0;
 }
 
-int sha256_finalize(void *ctx, gchar **hash){
-	const gchar *checksum_string = g_checksum_get_string(ctx);
+int
+sha256_finalize(void* ctx, gchar** hash)
+{
+	const gchar* checksum_string = g_checksum_get_string(ctx);
 	*hash = g_strdup(checksum_string);
 	return strlen(*hash);
 }
 
-int sha256_destroy(void *ctx){
+int
+sha256_destroy(void* ctx)
+{
 	g_checksum_free(ctx);
 	return 0;
 }
 
 jhash_algorithm hash_sha256 = {
-    sha256_context,
-    sha256_init,
-    sha256_update,
-    sha256_finalize,
+	sha256_context,
+	sha256_init,
+	sha256_update,
+	sha256_finalize,
 	sha256_destroy,
-    "SHA256",
-    J_HASH_SHA256
+	"SHA256",
+	J_HASH_SHA256
 };
-
 
 /**
  * @}
